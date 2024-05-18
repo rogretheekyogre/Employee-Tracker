855dbca9157e645fef9500f8af5174a8c7bd5f23
Roger_Aponte <rogeraponte10@gmail.com>
2024-05-17 20:01:12 -0400

    more js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const pool=require("./db/connection")
-const inquirer=require("inquirer")
-function menu(){
-inquirer.createPromptModule()
+const pool = require("./db/connection")
+const inquirer = require("inquirer")
+function menu() {
+    inquirer.prompt([{
+        type: "list", name: "choice", message: "what would you like to do?",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role", "quit"]
+
+    }]).then(({choice})=>{
+if(choice === "view all departments") viewDepartments()
+    else if(choice==="view all roles") viewRoles()
+else if(choice==="view all employees") viewEmployees()
+    else if(choice==="add a department") addDepartment()
+else if(choice==="add a role")
+
+    })
 
 }
